4f50d2e1f75479960d146f06a114c62b40cafe9b
gregSWEN <69919565+gregSWEN@users.noreply.github.com>
2022-01-26 09:48:55 -0500

    Time: 5 ms (54.72%), Space: 42.4 MB (25.70%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        
+        // First add all the nums to a hashtable so that we can look for the complimetn 
+        // Go through the nums in numbs again and see if the compliment is contained in the hashmap 
+        
+        Hashtable<Integer, Integer> table = new Hashtable<>();
+        int[] out = new int[2];
+        int compliment = 0;
+        for(int i = 0; i < nums.length; i++){
+            compliment = target - nums[i];
+            if(table.containsKey(compliment)){
+                out[0] = table.get(compliment);
+                out[1] = i;
+                return out;
+            }
+            table.put(nums[i], i);   
+        }
+        
+        return null;
+    }
+}
