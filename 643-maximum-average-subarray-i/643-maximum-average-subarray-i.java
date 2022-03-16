53bc2d5ddc68f4457db3fb512bc6aaf99e49e24a
Greg Villafane <69919565+gregSWEN@users.noreply.github.com>
2022-03-16 10:42:51 -0400

    Time: 8 ms (38.80%), Space: 107.5 MB (40.58%) - LeetHub

---
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        
+        double winSum = 0;
+        int winStart = 0;
+        double maxAverage = Integer.MIN_VALUE;
+        double currAverage = 0;
+        for(int winEnd=0; winEnd<nums.length; winEnd++){
+            winSum += nums[winEnd];
+            
+            
+            if(winEnd >= k - 1){ // If we get to the end of the first window
+                currAverage = winSum/k;
+                if(currAverage>maxAverage){
+                    maxAverage = currAverage;
+                }
+                winSum -= nums[winStart]; // Subtract the element leaving the window
+                winStart++;
+            }
+        }
+        return maxAverage;
+        
+    }
+}
